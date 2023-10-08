8069ce9c71a39cf468a6c0dacf4209efb72e9147
JCSALearn <jcsa.learning@outlook.com>
2023-10-08 14:50:55 +0100

    getComputerChoice function created

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const a = "Hello World";
+const option = ["Rock" , "Paper" , "Scissors"];
+function getComputerChoice () {
+    
+    let getChoice = Math.floor((Math.random()*100)+1);
+    console.log(getChoice)
+    if (getChoice <= 33){
+        return option[0];
+    } else if (getChoice >= 66) {
+        return option[2];
+    } else {
+        return option[1]
+    }
+}
+
+function gameOn(playerSelection, ComputerSelection) {
+
+}
