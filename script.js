85667810585496f86e9f9f892468a996177c0879
JCSALearn <jcsa.learning@outlook.com>
2023-10-09 19:46:42 +0100

    game function created, game is fully functional

--- script.js
+++ script.js
@@ -1,55 +1,82 @@
 //global 
-const option = ["Rock" , "Paper" , "Scissors"];
+let computer = 0;
+let human = 0;
+
 
 function getComputerChoice () {
     
     let getChoice = Math.floor((Math.random()*100)+1);
-    console.log(getChoice)
     if (getChoice <= 33){
-        return option[0];
+        return "Rock";
     } else if (getChoice >= 66) {
-        return option[2];
+        return "Scissors";
     } else {
-        return option[1]
+        return "Paper"
     }
 }
 
-function gameOn(playerSelection, computerSelection) {
+function playerChoice() {
+    let playerSelection = prompt("Rock, Paper Scissors?")
+    return playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
     
     if (playerSelection == computerSelection) {
         return console.log("It's a Draw!!!");
-    } else if (playerSelection == option[0]) {
-        
-        //Rock vs paper and Scissors
+
+//Rock vs paper and Scissors
+    } else if (playerSelection == "Rock") {
         switch (computerSelection){ 
-            case option[1]:
+            case "Paper":
                 console.log("You Loose!");
+                computer++;
                 break;
-            case option[2]:
+            case "Scissors":
                 console.log("YATAAAAA! You Win");
+                human++;
                 break;
         }
 
-        // Paper vs Scissors and Rock
-    } else if (playerSelection == option[1]) {
+// Paper vs Scissors and Rock
+    } else if (playerSelection == "Paper") {
         switch (computerSelection){ 
-            case option[2]:
+            case "Scissors":
                 console.log("You Loose!");
+                computer++;
                 break;
-            case option[0]:
+            case "Rock":
                 console.log("YATAAAAA! You Win");
+                human++;
                 break;
         }
 
-        // Scissors vs Rock and Paper
-    }else if (playerSelection == option[2]) {
+// Scissors vs Rock and Paper
+    }else if (playerSelection == "Scissors") {
         switch (computerSelection){ 
-            case option[0]:
+            case "Rock":
                 console.log("You Loose!");
+                computer++;
                 break;
-            case option[1]:
+            case "Paper":
                 console.log("YATAAAAA! You Win");
+                human++;
                 break;
         }
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5 ; i++) {
+    playRound(playerChoice(), getComputerChoice());
+    }
+    if (human > computer) {
+        console.log("Human Wins");
+        computer = 0;
+        human = 0;
+    } else {
+        console.log("Computer Wins");
+        human, computer = 0;
+        human = 0;
     }
 }
