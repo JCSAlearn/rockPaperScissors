c85acbe6027bcb0dc8f31594658c29f838b4706f
JCSALearn <jcsa.learning@outlook.com>
2023-10-09 17:45:53 +0100

    gameOn() function completed

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+//global 
 const option = ["Rock" , "Paper" , "Scissors"];
+
 function getComputerChoice () {
     
     let getChoice = Math.floor((Math.random()*100)+1);
@@ -12,6 +14,42 @@ function getComputerChoice () {
     }
 }
 
-function gameOn(playerSelection, ComputerSelection) {
+function gameOn(playerSelection, computerSelection) {
+    
+    if (playerSelection == computerSelection) {
+        return console.log("It's a Draw!!!");
+    } else if (playerSelection == option[0]) {
+        
+        //Rock vs paper and Scissors
+        switch (computerSelection){ 
+            case option[1]:
+                console.log("You Loose!");
+                break;
+            case option[2]:
+                console.log("YATAAAAA! You Win");
+                break;
+        }
 
+        // Paper vs Scissors and Rock
+    } else if (playerSelection == option[1]) {
+        switch (computerSelection){ 
+            case option[2]:
+                console.log("You Loose!");
+                break;
+            case option[0]:
+                console.log("YATAAAAA! You Win");
+                break;
+        }
+
+        // Scissors vs Rock and Paper
+    }else if (playerSelection == option[2]) {
+        switch (computerSelection){ 
+            case option[0]:
+                console.log("You Loose!");
+                break;
+            case option[1]:
+                console.log("YATAAAAA! You Win");
+                break;
+        }
+    }
 }
